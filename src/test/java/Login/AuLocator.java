fae47e03406710541f1dd443454bc720f3351cac
Alurujayaprakashreddy434 <alurujayaprakashreddy@gmail.com>
2025-06-05 00:18:03 +0530

    2th commit

---
+++ src/test/java/Login/AuLocator.java
@@ -0,0 +1,21 @@
+package Login;
+
+import java.nio.file.Path;
+
+public class AuLocator {
+
+	public class ScreenshotOptions {
+
+		public Object setPath(Path path) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+	public void screenshot(Object setPath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/test/java/Login/LoginTest.java
+++ src/test/java/Login/LoginTest.java
@@ -1,7 +1,16 @@
 package Login;
 
-import org.openqa.selenium.WebDriver;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.*;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.*;
 
@@ -12,19 +21,37 @@ public class LoginTest {
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void loginToOrangeHRM() {
+    public void loginToOrangeHRM() throws IOException {
         driver.get("https://opensource-demo.orangehrmlive.com/");
-        System.out.println("Title: " + driver.getTitle());
-        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Login
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username"))).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        // Wait for dashboard to load
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Dashboard']")));
+
+        // Click Admin tab
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Admin']"))).click();
+
+        // Optional: Screenshot
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        Files.createDirectories(Paths.get("screenshots"));
+        FileHandler.copy(src, new File("screenshots/admin_page.png"));
+
+        System.out.println("Test completed and screenshot saved.");
     }
 
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
-
-// This is for testing 
